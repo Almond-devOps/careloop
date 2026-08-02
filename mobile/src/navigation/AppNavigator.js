e415aff8b14f3fa1e474bcdf782fb5deb39083ec
Almond-devOps <jimohabdulmueez83@gmail.com>
2026-08-02 19:50:41 +0100

    Implement AppNavigator with stack navigation

--- mobile/src/navigation/AppNavigator.js
+++ mobile/src/navigation/AppNavigator.js
@@ -1,1 +1,34 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import LogScreen from "../screens/LogScreen";
+import HistoryScreen from "../screens/HistoryScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "#F4F1EC" },
+          headerShadowVisible: false,
+          headerTintColor: "#2E2A24",
+        }}
+      >
+        <Stack.Screen
+          name="Log"
+          component={LogScreen}
+          options={{
+            title: "CareLoop",
+            headerRight: () => null,
+          }}
+        />
+        <Stack.Screen name="History" component={HistoryScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
